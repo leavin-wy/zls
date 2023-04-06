8218aaf1f9964a91e460103960750d1699a8be77
leavin-wy <45867836+leavin-wy@users.noreply.github.com>
2023-04-06 17:53:20 +0800

    Add files via upload

---
+++ wy-generator/src/main/java/com/javaweb/generator/constant/GenTableConstant.java
@@ -0,0 +1,16 @@
+package com.javaweb.generator.constant;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <p>
+ * 代码生成 模块常量
+ * </p>
+ *
+ * @author leavin
+ * @since 2020-05-10
+ */
+public class GenTableConstant {
+
+}
