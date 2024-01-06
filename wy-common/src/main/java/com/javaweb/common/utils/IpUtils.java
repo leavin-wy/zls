433233511aeff4cadc669f1d49507844340e750f
leavin-wy <wuyua2008@126.com>
2024-01-06 23:55:17 +0800

    修改代码

--- wy-common/src/main/java/com/javaweb/common/utils/IpUtils.java
+++ wy-common/src/main/java/com/javaweb/common/utils/IpUtils.java
@@ -1,18 +1,13 @@
 package com.javaweb.common.utils;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.google.gson.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.util.StringUtils;
-import sun.applet.Main;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.*;
 
@@ -52,11 +47,6 @@ public class IpUtils {
             ip = request.getRemoteAddr();
         }
         return "0:0:0:0:0:0:0:1".equals(ip) ? "127.0.0.1" : ip;
-    }
-
-    public static boolean internalIp(String ip) {
-        byte[] addr = textToNumericFormatV4(ip);
-        return internalIp(addr) || "127.0.0.1".equals(ip);
     }
 
     private static boolean internalIp(byte[] addr) {
@@ -172,128 +162,6 @@ public class IpUtils {
         return "未知";
     }
 
-    /**
-     * 通过IP地址获取MAC地址
-     *
-     * @param ip String,127.0.0.1格式
-     * @return mac String
-     * @throws Exception
-     */
-    public String getMACAddress(String ip) throws Exception {
-        String line = "";
-        String macAddress = "";
-        final String MAC_ADDRESS_PREFIX = "MAC Address = ";
-        final String LOOPBACK_ADDRESS = "127.0.0.1";
-        //如果为127.0.0.1,则获取本地MAC地址。
-        if (LOOPBACK_ADDRESS.equals(ip)) {
-            InetAddress inetAddress = InetAddress.getLocalHost();
-            //貌似此方法需要JDK1.6。
-            byte[] mac = NetworkInterface.getByInetAddress(inetAddress).getHardwareAddress();
-            //下面代码是把mac地址拼装成String
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < mac.length; i++) {
-                if (i != 0) {
-                    sb.append("-");
-                }
-                //mac[i] & 0xFF 是为了把byte转化为正整数
-                String s = Integer.toHexString(mac[i] & 0xFF);
-                sb.append(s.length() == 1 ? 0 + s : s);
-            }
-            //把字符串所有小写字母改为大写成为正规的mac地址并返回
-            macAddress = sb.toString().trim().toUpperCase();
-            return macAddress;
-        }
-        //获取非本地IP的MAC地址
-        try {
-            Process p = Runtime.getRuntime().exec("nbtstat -A " + ip);
-            InputStreamReader isr = new InputStreamReader(p.getInputStream());
-            BufferedReader br = new BufferedReader(isr);
-            while ((line = br.readLine()) != null) {
-                if (line != null) {
-                    int index = line.indexOf(MAC_ADDRESS_PREFIX);
-                    if (index != -1) {
-                        macAddress = line.substring(index + MAC_ADDRESS_PREFIX.length()).trim().toUpperCase();
-                    }
-                }
-            }
-            br.close();
-        } catch (IOException e) {
-            e.printStackTrace(System.out);
-        }
-        return macAddress;
-    }
-
-    /**
-     * 通过IP获取地址(需要联网，调用淘宝的IP库)
-     *
-     * @param ip IP地址
-     * @return
-     */
-    public static String getIpInfo(String ip) {
-        if ("127.0.0.1".equals(ip)) {
-            ip = "127.0.0.1";
-        }
-        String info = "";
-        try {
-            URL url = new URL("http://ip.taobao.com/service/getIpInfo.php?ip=" + ip);
-            HttpURLConnection htpcon = (HttpURLConnection) url.openConnection();
-            htpcon.setRequestMethod("GET");
-            htpcon.setDoOutput(true);
-            htpcon.setDoInput(true);
-            htpcon.setUseCaches(false);
-
-            InputStream in = htpcon.getInputStream();
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
-            StringBuffer temp = new StringBuffer();
-            String line = bufferedReader.readLine();
-            while (line != null) {
-                temp.append(line).append("\r\n");
-                line = bufferedReader.readLine();
-            }
-            bufferedReader.close();
-            JSONObject obj = (JSONObject) JSON.parse(temp.toString());
-            if (obj.getIntValue("code") == 0) {
-                JSONObject data = obj.getJSONObject("data");
-                info += data.getString("country") + " ";
-                info += data.getString("region") + " ";
-                info += data.getString("city") + " ";
-                info += data.getString("isp");
-            }
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (ProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return info;
-    }
-
-    /**
-     * 根据IP查询地区
-     *
-     * @param ip IP地址
-     * @return
-     */
-    public static String getRealAddressByIP(String ip) {
-        String address = "XX XX";
-        // 内网不查询
-        if (IpUtils.internalIp(ip)) {
-            return "内网IP";
-        }
-        String rspStr = HttpUtils.sendPost("http://ip.taobao.com/service/getIpInfo.php", "ip=" + ip);
-        if (StringUtils.isEmpty(rspStr)) {
-            log.error("获取地理位置异常 {}", ip);
-            return address;
-        }
-        JSONObject obj = JSONObject.parseObject(rspStr);
-        JSONObject data = obj.getObject("data", JSONObject.class);
-        String region = data.getString("region");
-        String city = data.getString("city");
-        address = region + " " + city;
-        return address;
-    }
-
     public static String getAddressByIp(String ip) {
         String location = "内网";
         try {
@@ -306,8 +174,10 @@ public class IpUtils {
                 result.append(line);
             }
             reader.close();
+            log.info("ip信息：" + result.toString());
             JSONObject jsStr = JSONObject.parseObject(result.toString());
             JSONArray jsData = (JSONArray) jsStr.get("data");
+            if(jsData.size()<1) return ip;
             JSONObject data= (JSONObject) jsData.get(0);
             location = (String) data.get("location");
             log.info("登录ip地址:{}",location);
